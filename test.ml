35f0b3b6a2ef5688e9ceb49360163bc3c0ff4c99
Cedric Cellier <rixed@happyleptic.org>
2011-02-16 19:27:47 +0100

    Make wrap mode configurable.

--- test.ml
+++ test.ml
@@ -10,22 +10,23 @@ let chk ok =
 		failwith "Error"
 	)
 
+let print x y c = if not (mvaddch y x (int_of_char c)) then errs := (Printf.sprintf "Cannot print at %d %d" x y)::!errs
+
+let set_palette p f b = chk (init_pair p f b)
+
 let init () =
 	ignore (initscr ()) ;
 	assert (has_colors ()) ; (* FIXME *)
 	chk (start_color ()) ;
-
-	chk (use_default_colors ()) ;
+	for c = 1 to (colors () - 1) do set_palette c c Color.black done ;
+	set_palette 2 Color.green Color.black ;
+	set_palette 3 Color.blue Color.black ;
 	chk (cbreak ()) ;
 	chk (noecho ()) ;
 	chk (curs_set 0)
 
 let exit () =
 	endwin ()
-
-let print x y c = if not (mvaddch y x (int_of_char c)) then errs := (Printf.sprintf "Cannot print at %d %d" x y)::!errs
-
-let set_palette p f b = chk (init_pair p f b)
 
 type split_dir = Horizontal | Vertical 
 type split_size = Absolute of int (* min abs size *) | Relative of float
@@ -47,19 +48,49 @@ class text_buf text =
 object
 	inherit buf
 	val text = text
-	val color = 0	(* By default, use default terminal color pair (white on black usually) *)
+	val mutable color = 0	(* By default, use default terminal color pair (white on black usually) *)
+	val mutable wrap_symbol_color = 0
+	val mutable wrap_lines = false
+
+	method set_wrap ?(symbol_color) w =
+		wrap_lines <- w ;
+		wrap_symbol_color <- optdef symbol_color color
+
 	(* val offset_x, offset_y, wrap_lines... *)
 	method display x0 y0 width height =
-		let put_chr (x, y) c =
+		let put_chr_nowrap (x, y) c =
 			if y >= height then raise Exit ;
 			if c = '\n' then (
-				for x = x to width-1 (* width-2 fonctionne?!*) do print (x+x0) (y+y0) ' ' done ;
+				for x = x to width-1 do print (x+x0) (y+y0) ' ' done ;
 				0, y+1
 			) else (
-				if x < width then print (x+x0) (y+y0) c ;
+				if x < width-1 then (
+					print (x+x0) (y+y0) c
+				) else if x = width-1 (* and not the last or next newline *) then (
+					attrset (A.color_pair wrap_symbol_color) ;
+					print (x+x0) (y+y0) '+' ;
+					attrset (A.color_pair color)
+				) ;
 				x+1, y
 			) in
-		attron (A.color_pair color) ;
+		let rec put_chr_wrap (x, y) c =
+			if y >= height then raise Exit ;
+			if c = '\n' then (
+				for x = x to width-1 do print (x+x0) (y+y0) ' ' done ;
+				0, y+1
+			) else (
+				if x < width-1 then (
+					print (x+x0) (y+y0) c ;
+					x+1, y
+				) else (
+					attrset (A.color_pair wrap_symbol_color) ;
+					print (x+x0) (y+y0) '\\' ;
+					attrset (A.color_pair color) ;
+					put_chr_wrap (0, y+1) c
+				)
+			) in
+		attrset (A.color_pair color) ;
+		let put_chr = if wrap_lines then put_chr_wrap else put_chr_nowrap in
 		try ignore (Rope.fold_left put_chr (0, 0) text)
 		with Exit -> ()
 end
@@ -118,8 +149,12 @@ let display_root root =
 	display 0 0 width height root
 
 let test () =
-	let root = Leaf (new text_buf (Rope.of_file "test.ml")) in
-	let root = split ~split_dir:Vertical (new text_buf (Rope.of_string "blabla")) (Relative 0.5) root in
+	let content = Rope.of_file "test.ml" in
+	let b1 = new text_buf content
+	and b2 = new text_buf content in
+	b1#set_wrap ~symbol_color:1 true ;
+	b2#set_wrap ~symbol_color:1 false ;
+	let root = split ~split_dir:Vertical (b2:>buf) (Relative 0.5) (Leaf (b1:>buf)) in
 	display_root root ;
 	chk (refresh ()) ;
 	(* Then wait a key *)
