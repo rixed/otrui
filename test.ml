0fbd952a7a855127ce1babd2c4e44c32e10d4fd1
Cedric Cellier <rixed@happyleptic.org>
2011-02-19 11:01:46 +0100

    Another version with working toplevel, still without communications between prog and toplevel env.

--- test.ml
+++ test.ml
@@ -1,4 +1,3 @@
-(* ------------------------------------------------------------------------------ *)
 open Bricabrac
 open Curses
 module Rope = Rope_impl.Make
@@ -26,7 +25,7 @@ let char_of_key k =
 	else if k > 255 then '?'
 	else char_of_int k
 
-let init () =
+let init_ncurses () =
 	Log.p "Initializing ncurses..." ;
 	ignore (initscr ()) ;
 	assert (has_colors ()) ; (* FIXME *)
@@ -47,14 +46,34 @@ let init () =
 	Log.p "  hide cursor..." ;
 	chk (curs_set 0)
 
-let exit () =
+let quit () =
 	Log.p "Exiting ncurses" ;
 	endwin ()
+
+(* Buffers are somewhat editable content. *)
+
+class virtual buffer =
+object
+	method virtual get : char Rope.t
+	method virtual set : char Rope.t -> unit
+end
+
+class text_buffer init_content =
+object
+	inherit buffer
+	val mutable content = init_content
+	method get = content
+	method set c = content <- c
+	method append c = content <- Rope.cat content c
+end
+
+(* A view is a window that can be opened/closed/moved/resized
+ * and which purpose is to display a buffer *)
 
 type split_dir = Horizontal | Vertical 
 type split_size = Absolute of int (* min abs size *) | Relative of float
 
-class virtual buf =
+class virtual view =
 object
 	method virtual display : int -> int -> int -> int -> unit
 	(* Et une autre pour interpreter une commande... c'est à dire qu'on peut appeler directement
@@ -62,18 +81,9 @@ object
 	method virtual key : int -> unit
 end
 
-class unbound_buf =
+class text_view buf =
 object
-	inherit buf
-	method display _x0 _y0 _width _height = ()
-	method key _k = ()
-end
-
-class text_buf text =
-object
-	inherit buf
-	val mutable text = text
-	method set_text t = text <- t 	(* TODO: save previous versions for undo *)
+	inherit view
 	val mutable color = 0	(* By default, use default terminal color pair (white on black usually) *)
 	val mutable wrap_symbol_color = 0
 	val mutable wrap_lines = false
@@ -117,53 +127,16 @@ object
 			) in
 		attrset (A.color_pair color) ;
 		let put_chr = if wrap_lines then put_chr_wrap else put_chr_nowrap in
-		try ignore (Rope.fold_left put_chr (0, 0) text)
+		try ignore (Rope.fold_left put_chr (0, 0) buf#get)
 		with Exit -> ()
 
-	method key _k = ()
-end
-
-class pipe_buf ch_in ch_out =
-	let text = Rope.empty in
-object (self)
-	inherit text_buf text
-	val ch_in = ch_in
-	val ch_out = ch_out
-	val mutable last_len = 0
 	method key k =
 		let c = char_of_key k in
-		self#set_text (Rope.cat text (Rope.singleton c)) ;
-		if c = '\n' then (
-			let len = Rope.length text in
-			let last_cmd = Rope.sub text last_len len in
-			last_len <- len ;
-			output_string ch_out (Rope.to_string last_cmd) ;
-			flush ch_out ;
-			let resp = input_line ch_in in
-			self#set_text (Rope.cat text (Rope.of_string resp))
-		)
+		buf#append (Rope.singleton c)
 end
-
-class cmd_buf submit_cmd =
-	let text = Rope.empty in
-object (self)
-	inherit text_buf text
-	val mutable last_len = 0
-	method key k =
-		let c = char_of_key k in
-		self#set_text (Rope.cat text (Rope.singleton c)) ;
-		if c = '\n' then (
-			let len = Rope.length text in
-			let last_cmd = Rope.sub text last_len len in
-			let result = submit_cmd last_cmd in
-			self#set_text (Rope.cat text result) ;
-			last_len <- Rope.length text ;
-		)
-end
-
 
 type win =
-	| Leaf of buf
+	| Leaf of view
 	| Split of (split_dir * (split_size * win) list)
 
 let win_sub_sizes children =
@@ -175,20 +148,20 @@ let win_sub_sizes children =
 
 let default_split_dir = Horizontal
 
-let split ?(split_dir=default_split_dir) buf size win =
-	Split (split_dir, [ size, Leaf buf ; Relative 1., win ])
+let split ?(split_dir=default_split_dir) view size win =
+	Split (split_dir, [ size, Leaf view ; Relative 1., win ])
 
-let resplit buf size = function
+let resplit view size = function
 	| Leaf _ -> failwith "Cannot resplit leaf window"
 	| Split (split_dir, children) ->
-		Split (split_dir, (size, Leaf buf) :: children)
+		Split (split_dir, (size, Leaf view) :: children)
 
 let size_of rem_abs = function
 	| Absolute a -> min a rem_abs
 	| Relative r -> int_of_float ((float_of_int rem_abs) *. r)
 
 let rec display x0 y0 width height = function
-	| Leaf buf -> buf#display x0 y0 width height
+	| Leaf view -> view#display x0 y0 width height
 	| Split (dir, children) ->
 		let rec aux c = function
 			| [] -> failwith "should not happen"
@@ -206,59 +179,92 @@ let rec display x0 y0 width height = function
 				aux next_c children' in
 		aux 0 children
 
-let display_root root =
-	let win = stdscr () in
-	let height, width = getmaxyx win in
-	display 0 0 width height root
+let repl = new text_buffer Rope.empty
 
-let _ =
-	Toploop.initialize_toplevel_env()
-
-let rope_formatter rope_ref =
-	let out str pos len =
-		let s = String.sub str pos len in
-		let r = Rope.of_string s in
-		rope_ref := Rope.cat !rope_ref r in
-	Format.make_formatter out nop
-
-let submit_toplevel q =
-	let query = Rope.to_string q in
-	let lb = Lexing.from_string query in
-	let phr = !Toploop.parse_toplevel_phrase lb in
-	let rope_ref = ref Rope.empty in
-	let ok = Toploop.execute_phrase true (rope_formatter rope_ref) phr in
-	Log.p "Toplevel: %s -> %s (%b)" query (Rope.to_string !rope_ref) ok ;
-	!rope_ref
-
-let test () =
+let root =
 	let height, width = getmaxyx (stdscr ()) in
 	Log.p "Total resolution = %d x %d" width height ;
 	Log.p "Max colors = %d" (colors ()) ;
 	Log.p "Max color pairs = %d" (color_pairs ()) ;
-	let content = Rope.of_file "test.ml" in
-	let b1 = new text_buf content
-	and b2 = new text_buf content in
-	b1#set_wrap ~symbol_color:1 true ;
-	b2#set_wrap ~symbol_color:1 false ;
-	let top = split ~split_dir:Vertical (b2:>buf) (Relative 0.5) (Leaf (b1:>buf)) in
-	let repl = new cmd_buf submit_toplevel in
-	let root = split (repl:>buf) (Absolute 10) top in
-	(* read keys in loop *)
-	let focused = repl in
-	let rec next_key () =
-		display_root root ;
-		chk (refresh ()) ;
-		let k = getch () in
-		if k = 27 (* ESC *) then (
-			exit ();
-		) else (
-			focused#key k ;
-			next_key ()
-		) in
-	next_key ()
+	let content = new text_buffer (Rope.of_file "test.ml") in
+	let v1 = new text_view content
+	and v2 = new text_view content in
+	v1#set_wrap ~symbol_color:1 true ;
+	v2#set_wrap ~symbol_color:1 false ;
+	let top = split ~split_dir:Vertical (v2:>view) (Relative 0.5) (Leaf (v1:>view)) in
+	let repl_view = new text_view repl in
+	split (repl_view:>view) (Absolute 10) top
 
-let main =
-	init () ;
-	(* Do not write into repl_in until the toplevel is actually reading, or we will deadblock.
-	 * The solution here is to start a new thread *)
-	try_finalize test () exit ()
+let display_root () =
+	let win = stdscr () in
+	let height, width = getmaxyx win in
+	display 0 0 width height root
+
+let focused =
+	let rec get_first_view = function
+		| [] -> failwith "No views?"
+		| (_, Leaf view) :: _ -> view
+		| (_, Split (_, children)) :: l -> get_first_view (children @ l) in
+	get_first_view [Relative 1., root]
+
+let ends_with e r =
+	let lr = Rope.length r in
+	let le = String.length e in
+	if lr >= le then (
+		let e' = Rope.to_string (Rope.sub r (lr-le) lr) in
+		e' = e
+	) else false
+
+let repl_len = ref 0
+(* toploop can write into repl independantly *)
+let read_for_toploop prompt buffer buffer_len =
+	let fill_buffer () =
+		(* Copy into buffer as much as we can *)
+		let len = Rope.length repl#get in
+		let nb_items = min (len - !repl_len) buffer_len in
+		let cmd = Rope.sub repl#get !repl_len (!repl_len + nb_items) in
+		Log.p "Got a new command: '%s'" (Rope.to_string cmd) ;
+		Rope.iteri (String.set buffer) cmd ;
+		repl_len := !repl_len + nb_items ;
+		nb_items, false in
+	(* If we have a prompt, then we save the length after the prompt *)
+	if prompt = "" && Rope.length repl#get > !repl_len then (
+		Log.p "There was some content left in repl for the toplevel" ;
+		fill_buffer ()
+	) else (
+		Log.p "Loop for getch until something is ready for the toplevel" ;
+		repl#append (Rope.of_string prompt) ;
+		repl_len := Rope.length repl#get ;
+		(* read keys in loop, monitoring repl buffer for new content to send to toploop *)
+		let rec next_key () =
+			display_root () ;
+			chk (refresh ()) ;
+			let k = getch () in
+			if k = 27 (* ESC *) then (
+				quit () ;
+				exit 0
+			) else (
+				focused#key k ;
+				if k = 13 && Rope.length repl#get > !repl_len && ends_with ";;\n" repl#get then
+					fill_buffer ()
+				else next_key ()
+			) in
+		next_key ()
+	)
+
+let start_loop =
+	init_ncurses () ;
+	(* Input for toplevel must be talen from our special toplevel buf *)
+	Toploop.read_interactive_input := read_for_toploop ;
+	(* And output from toplevel should be redirected into this buf *)
+	let rope_formatter buf =
+		let out str pos len =
+			let s = String.sub str pos len in
+			let r = Rope.of_string s in
+			Log.p "Toplevel formatting output '%s'" s ;
+			buf#append r in
+		Format.make_formatter out nop in
+	let toploop_formatter = rope_formatter repl in
+	(* Load otrui *)
+	Toploop.loop toploop_formatter
+
