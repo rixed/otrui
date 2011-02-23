93958c71c2e5994aa6cfff9232c099913330a466
Cedric Cellier <rixed@happyleptic.org>
2011-02-23 22:53:18 +0100

    Vertical scrolling, delete key, status line...

--- test.ml
+++ test.ml
@@ -15,6 +15,10 @@ let chk ok =
 	)
 
 let print x y c = if not (mvaddch y x (int_of_char c)) then Log.p "Cannot print at %d %d" x y
+let print_string x y str =
+	for i = 0 to (String.length str) - 1 do
+		print (x+i) y str.[i]
+	done
 
 let set_palette p f b = chk (init_pair p f b)
 
@@ -32,8 +36,6 @@ let init_ncurses () =
 	chk (start_color ()) ;
 	Log.p "  Initializing some color pairs..." ;
 	for c = 1 to (colors () - 1) do set_palette c c Color.black done ;
-	set_palette 2 Color.green Color.black ;
-	set_palette 3 Color.blue Color.black ;
 	Log.p "  cbreak..." ;
 	chk (cbreak ()) ;
 	Log.p "  noecho..." ;
@@ -43,7 +45,11 @@ let init_ncurses () =
 	Log.p "  keypad..." ;
 	chk (keypad (stdscr ()) true) ;
 	Log.p "  hide cursor..." ;
-	chk (curs_set 0)
+	chk (curs_set 0) ;
+	let height, width = getmaxyx (stdscr ()) in
+	Log.p "  resolution = %d x %d" width height ;
+	Log.p "  max colors = %d" (colors ()) ;
+	Log.p "  max color pairs = %d" (color_pairs ())
 
 let quit () =
 	Log.p "Exiting ncurses" ;
@@ -54,15 +60,17 @@ let quit () =
 class virtual buffer =
 object
 	method virtual get : char Rope.t
+	method virtual name : string
 end
 
 type text_buffer_mark = { mutable mark_pos: int }
 
-class text_buffer init_content =
+class text_buffer init_content name =
 object
 	inherit buffer
 	val mutable content = init_content
 	method get = content
+	method name = name
 
 	val mutable marks = []
 	(* Return a new mark at this position *)
@@ -98,7 +106,7 @@ end
 let repl =
 	let prompt = Rope.of_string "# " in
 object
-	inherit text_buffer prompt as parent
+	inherit text_buffer prompt "REPL" as parent
 
 	val mutable resp_end = { mark_pos = 0 }	(* not really *)
 	initializer
@@ -137,7 +145,6 @@ object
 		)
 end
 
-
 (* A view is a window that can be opened/closed/moved/resized
  * and which purpose is to display a buffer *)
 
@@ -148,6 +155,7 @@ let views = Hashtbl.create 20
 let view_list () = Hashtbl.fold (fun n _ p -> n::p) views []
 let get_view name = Hashtbl.find views name
 
+
 class virtual view (name:string) =
 object (self)
 	initializer Hashtbl.add views name (self :> view)
@@ -156,6 +164,8 @@ object (self)
 	(* Et une autre pour interpreter une commande... c'est à dire qu'on peut appeler directement
 	 * une autre méthode pour modifier l'état interne de buffer... *)
 	method virtual key : int -> unit
+	method virtual content_descr  : string (* left justified *)
+	method virtual content_status : string (* right justified *)
 end
 
 class text_view name ?(append=false) buf =
@@ -170,7 +180,17 @@ class text_view name ?(append=false) buf =
 			if Rope.nth buf#get pos = '\n' then off
 			else aux (off+1) (pos+1) in
 		aux 0 pos in
-
+	let nb_lines_between pos1 pos2 =
+		let s = Rope.sub buf#get pos1 pos2 in
+		Rope.fold_left (fun l c -> if c = '\n' then l+1 else l) 0 s in
+	let more_lines_than n pos =	(* Check that there are more than n lines from pos to end *)
+		let s = Rope.sub buf#get pos (Rope.length buf#get) in
+		try ignore (Rope.fold_left (fun l c ->
+			if c = '\n' then (if l >= n then raise Exit else l+1)
+			else l) 1 s) ;
+			false
+		with Exit -> true
+	in
 object (self)
 	inherit view name
 	val mutable color = 0	(* By default, use default terminal color pair (white on black usually) *)
@@ -182,10 +202,41 @@ object (self)
 		wrap_lines <- w ;
 		wrap_symbol_color <- optdef symbol_color color
 
+	val mutable pos_first_line = buf#mark 0 (* offset in buf of the first char of the first displayed line *)
 	val mutable cursor = buf#mark (if append then Rope.length buf#get else 0)
+	(* We start to scroll vertically if the cursor is less than this number of lines away
+	 * from window border *)
+	val mutable start_scroll_margin_y = 3
+	val mutable start_scroll_margin_x = 5
+	(* Reset starting position of display according to start_scroll_margin_y *)
+	method center_cursor_y height margin =
+		assert (height >= 1) ;
+		(* scroll up *)
+		while
+			let cursor_y = nb_lines_between pos_first_line.mark_pos cursor.mark_pos in
+			cursor_y < margin && pos_first_line.mark_pos > 0
+		do
+			pos_first_line.mark_pos <-
+				let prev_line_len = from_line_start (pos_first_line.mark_pos-1) in
+				pos_first_line.mark_pos - 1 - prev_line_len
+		done ;
+		assert (pos_first_line.mark_pos <= cursor.mark_pos) ;
+		(* scroll down *)
+		while
+			let cursor_y' = height - 1 - nb_lines_between pos_first_line.mark_pos cursor.mark_pos in
+			cursor_y' < margin && more_lines_than height pos_first_line.mark_pos
+		do
+			pos_first_line.mark_pos <-
+				pos_first_line.mark_pos + to_line_end pos_first_line.mark_pos + 1
+		done
+
+	method content_descr = buf#name
+	method content_status = "[--X---]"	(* TODO *)
 
 	(* val offset_x, pos_first_line... *)
 	method display x0 y0 width height =
+		Log.p "display %s from %d,%d, width=%d, height=%d" buf#name x0 y0 width height ;
+		self#center_cursor_y height start_scroll_margin_y ;
 		let rec put_chr (x, y, n) c =
 			if y >= height then raise Exit ;
 			attrset (if n = cursor.mark_pos then A.reverse else A.normal) ;
@@ -219,9 +270,10 @@ object (self)
 			) in
 		attrset (A.color_pair color) ;
 		(* So that cursor is draw even when at the end of buffer *)
-		let buf_space = Rope.cat buf#get (Rope.singleton ' ') in
-		try
-			let x, y, _ = Rope.fold_left put_chr (0, 0, 0) buf_space in
+		let buf_eff = Rope.sub buf#get pos_first_line.mark_pos (Rope.length buf#get) in
+		let buf_eff = Rope.cat buf_eff (Rope.singleton ' ') in
+		(try
+			let x, y, _ = Rope.fold_left put_chr (0, 0, pos_first_line.mark_pos) buf_eff in
 			(* deletes the rest of the buffer *)
 			let rec aux x y =
 				if y < height then (
@@ -232,7 +284,7 @@ object (self)
 				) in
 			attrset (A.color_pair no_content_color) ;
 			aux x y
-		with Exit -> () ;
+		with Exit -> ()) ;
 
 	method beep = ()
 
@@ -264,6 +316,9 @@ object (self)
 		) else if k = Key.backspace then (
 			if cursor.mark_pos == 0 then self#beep
 			else buf#delete (cursor.mark_pos-1) 1
+		) else if k = Key.dc then (	(* delete *)
+			if cursor.mark_pos == Rope.length buf#get then self#beep
+			else buf#delete (cursor.mark_pos) 1
 		) else (
 			(* Other keys *)
 			let c = char_of_key k in
@@ -300,8 +355,31 @@ let size_of rem_abs = function
 	| Absolute a -> min a rem_abs
 	| Relative r -> int_of_float ((float_of_int rem_abs) *. r)
 
+let status_color = ref 3
+let display_with_status view x0 y0 width height =
+	let display_status view x0 y0 width =
+		let descr  = view#content_descr
+		and status = view#content_status in
+		let descr_len  = String.length descr
+		and status_len = String.length status in
+		attrset (A.color_pair !status_color) ;
+		for i = 0 to width - 1 do print (x0+i) y0 '-' done ;
+		if width >= status_len then (
+			print_string (x0+width-status_len) y0 status ;
+			let width = width - status_len - 1 in
+			if width >= descr_len then
+				print_string x0 y0 descr
+			else
+				print_string x0 y0 (String.sub descr 0 width)
+		) in
+	assert (height >= 1) ;
+	if height > 1 then view#display x0 y0 width (height-1) ;
+	display_status view x0 (y0+height-1) width
+
+let show_vert_split = ref true
+let vert_split_color = ref !status_color
 let rec display x0 y0 width height = function
-	| Leaf view -> view#display x0 y0 width height
+	| Leaf view -> display_with_status view x0 y0 width height
 	| Split (dir, children) ->
 		let rec aux c = function
 			| [] -> failwith "should not happen"
@@ -314,17 +392,23 @@ let rec display x0 y0 width height = function
 				let x0, y0, width, height = match dir with
 				| Horizontal -> x0, c, width, size_of (height - c) sz
 				| Vertical -> c, y0, size_of (width - c) sz, height in
-				if width > 0 && height > 0 then display x0 y0 width height win ;
+				if width > 0 && height > 0 then (
+					if dir = Vertical && !show_vert_split then (
+						if width > 1 then display x0 y0 (width-1) height win ;
+						attrset (A.color_pair !vert_split_color) ;
+						for y = 0 to (height-1) do
+							print (x0+width-1) (y0+y) '|'
+						done
+					) else (
+						display x0 y0 width height win
+					)
+				) ;
 				let next_c = match dir with Horizontal -> y0 + height | Vertical -> x0 + width in
 				aux next_c children' in
 		aux 0 children
 
 let root =
-	let height, width = getmaxyx (stdscr ()) in
-	Log.p "Total resolution = %d x %d" width height ;
-	Log.p "Max colors = %d" (colors ()) ;
-	Log.p "Max color pairs = %d" (color_pairs ()) ;
-	let content = new text_buffer (Rope.of_file "test.ml") in
+	let content = new text_buffer (Rope.of_file "test.ml") "test.ml" in
 	let v1 = new text_view "view1" content
 	and v2 = new text_view "view2" content in
 	v1#set_wrap ~symbol_color:1 true ;
@@ -361,7 +445,7 @@ let key_loop () =
 		) in
 	next_key ()
 
-(* works only if a function... :-/ *)
+(* works only if a function... If not, cannot access to these symbols :-/ *)
 let start () =
 	init_ncurses () ;
 	key_loop ()
