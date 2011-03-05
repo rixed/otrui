d764f4f8cdd0baebd3f57ece51a0dadf6d5b2839
cedric cellier <rixed@free.fr>
2011-03-05 09:58:07 +0100

    Ignore sigpipe in pipe plugin.

--- plugins/pipe.ml
+++ plugins/pipe.ml
@@ -7,6 +7,8 @@ open Buf
 
 let _ = Repl.repl#append (Rope.of_string "Loading pipe buffer\n")
 
+let ignore_sig_child = Sys.set_signal Sys.sigpipe Sys.Signal_ignore
+
 class t program =
 	let prompt = Rope.of_string ">% "
 	and env = Unix.environment () in
@@ -24,7 +26,9 @@ object (self)
 			let line = Rope.cat (Rope.of_string line) (Rope.singleton '\n') in
 			let pos = resp_end.pos + 1 - (Rope.length prompt) in
 			parent#insert pos line in
-		forever aux ()
+		try forever aux ()
+		with End_of_file ->
+			Log.p "program '%s' exited" program
 
 	initializer
 		resp_end <- parent#mark (Rope.length prompt -1) ;
@@ -42,17 +46,22 @@ object (self)
 	method insert pos c =
 		let appending = pos = Rope.length content in
 		parent#insert pos c ;
-		if appending &&
-		   Rope.length content > 0 &&
-		   Rope.nth content ((Rope.length content) -1) = '\n' then (
+		if
+			appending &&
+			Rope.length content > 0 &&
+			Rope.nth content ((Rope.length content) -1) = '\n'
+		then (
 			let cur_len = Rope.length content in
 			assert (resp_end.pos < cur_len) ;
 			let input = Rope.sub content (resp_end.pos+1) cur_len in
 			let input = Rope.to_string input in
 			Log.p "Sending string '%s' to program '%s'" input program ;
-			output_string ch_out input ;
-			flush ch_out ;
-			self#append_prompt
+			try
+				output_string ch_out input ;
+				flush ch_out ;
+				self#append_prompt
+			with Sys_error str ->
+				Cmd.error str
 		)
 
 	(* Disallow to delete the last prompt *)
